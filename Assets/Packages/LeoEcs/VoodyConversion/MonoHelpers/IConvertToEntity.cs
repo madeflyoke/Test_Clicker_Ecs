d796f62d59a98bb326f4441a9d09060c08410466
alexander.hirych <sano555000@gmail.com>
2025-04-25 11:55:22 +0300

    fix: leoecs voody conversion removed

--- Assets/Packages/LeoEcs/VoodyConversion/MonoHelpers/IConvertToEntity.cs
+++
@@ -1,9 +0,0 @@
-using Leopotam.EcsLite;
-
-namespace Packages.LeoEcs.VoodyConversion.MonoHelpers
-{
-    public interface IConvertToEntity
-    {
-        void Convert(int entity, EcsWorld world);
-    }
-}
--- Assets/Packages/LeoEcs/VoodyConversion/MonoHelpers/MonoLink.cs
+++
@@ -1,8 +0,0 @@
-namespace Packages.LeoEcs.VoodyConversion.MonoHelpers
-{
-    [System.Serializable]
-    public struct MonoLink<TComponent> where TComponent : UnityEngine.Component
-    {
-        public TComponent Value;
-    }
-}
